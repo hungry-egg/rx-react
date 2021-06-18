70c781e48360bf36c38bfe6845d1417605716042
Mark Evans <markevans3000@gmail.com>
2021-06-18 15:06:24 +0100

    useButtons hook

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { ModuleContext } from "./contexts/ModuleContext";
 export { useAtom } from "./hooks/useAtom";
+export { useButtons } from "./hooks/useButtons";
 export { useRxState } from "./hooks/useRxState";
 export { useServices } from "./hooks/useServices";
 export { useStores } from "./hooks/useStores";
