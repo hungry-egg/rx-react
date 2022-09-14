710b102df7e519bacac9c4686c2c207e053f8a0d
Mark Evans <markevans3000@gmail.com>
2022-09-14 11:34:06 +0100

    exports correct

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-export { createModule } from "./utils/createModule";
-export { ModuleContext } from "./contexts/ModuleContext";
-export { useWrap } from "./hooks/useWrap";
-export { useButtons } from "./hooks/useButtons";
-export { useUnwrap } from "./hooks/useUnwrap";
-export { useModule, UseModule } from "./hooks/useModule";
-export { useSubscribe } from "./hooks/useSubscribe";
+export { useWrap } from "./useWrap";
+export { useUnwrap } from "./useUnwrap";
+export { useSubscribe } from "./useSubscribe";
